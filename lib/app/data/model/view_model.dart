ed9609a168db29a135dfc9f5e755f5d4d0091a11
namesace0427 <jaws02144@naver.com>
2022-10-22 16:44:58 +0900

    feat(post page):add post page

--- lib/app/data/model/view_model.dart
+++ lib/app/data/model/view_model.dart
@@ -21,19 +21,6 @@ class ViewModel {
     this.comments,
   });
 
-  // ViewModel.fromJson(Map<String, dynamic?> json) {
-  //   postId = json['postId'] ?? 0;
-  //   content = json['content'] ?? '123';
-  //   password = json['password'] ?? '123';
-  //   latitude = json['latitude'] ?? 0.0;
-  //   longitude = json['longitude'] ?? 0.0;
-  //   createdAt = json['createdAt'] ?? '123';
-  //   modifiedAt = json['modifiedAt'] ?? '123';
-  //   comments = json['comments']
-  //       .map((comment) => CommentModel.fromJson(comment))
-  //       .toList();
-  // }
-
   static List<CommentModel> makeCommentList(json) {
     return json.map((comment) => CommentModel.fromJson(comment)).toList();
   }
@@ -54,37 +41,6 @@ class ViewModel {
               json['comment'] == null ? [] : makeCommentList(json['comments']),
         );
 
-  // Content.fromJson(Map<String, Object?> json)
-  //     : this(
-  //         id: json['contentId'] == null ? 0 : json['contentId'] as int,
-  //         title: json['title'] == null ? '' : json['title'] as String,
-  //         subtitle: json['subtitle'] == null ? '' : json['subtitle'] as String,
-  //         content: json['content'] == null ? '' : json['content'] as String,
-  //         latitude: json['latitude'] == null ? 0 : json['latitude'] as double,
-  //         longitude:
-  //             json['longitude'] == null ? 0 : json['longitude'] as double,
-  //         address: json['address'] == null ? '' : json['address'] as String,
-  //         likes: json['likes'] == null ? 0 : json['likes'] as int,
-  //         articles: json['articles'] == null
-  //             ? []
-  //             : Content.articleListFromJson(json['articles']),
-  //         contentType:
-  //             json['contentType'] == null ? '' : json['contentType'] as String,
-  //         rating: json['rating'] == null ? 0 : json['rating'] as double,
-  //         blogReview:
-  //             json['blogReview'] == null ? 0 : json['blogReview'] as int,
-  //         visitorReview:
-  //             json['visitorReview'] == null ? 0 : json['visitorReview'] as int,
-  //         detailUrl: json['contentDetailUrl'] == null
-  //             ? ''
-  //             : json['contentDetailUrl'] as String,
-  //         phone:
-  //             json['phoneNumber'] == null ? '' : json['phoneNumber'] as String,
-  //         imageList: json['imageList'] == null
-  //             ? []
-  //             : Content.imageListFromJson(json['imageList']),
-  //         distance: json['distance'] == null ? 0.0 : json['distance'] as double,
-  //       );
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['postId'] = postId;
