a3e71ab68d7fa4ebd886957e0260f6cd6b197d59
namesace0427 <jaws02144@naver.com>
2022-10-30 17:25:23 +0900

    fix(viewpage, mypage): add reports logic

--- lib/app/ui/view_page.dart
+++ lib/app/ui/view_page.dart
@@ -3,8 +3,17 @@ import 'package:dawnline/app/ui/Widget/background_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:dawnline/app/controller/view_controller.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ViewPage extends GetView<ViewController> {
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  addreports(String postId) async {
+    SharedPreferences prefs = await _prefs;
+    List<String> list = prefs.getStringList('reports') ?? [];
+    list.add(postId);
+    prefs.setStringList('reports', list);
+  }
+
   @override
   Widget build(BuildContext context) {
     controller.getAll();
@@ -87,12 +96,52 @@ class ViewPage extends GetView<ViewController> {
                   children: [
                     IconButton(
                       icon: const Icon(
-                        Icons.my_library_books,
+                        Icons.list,
                         color: Colors.white,
                       ),
                       onPressed: () {
                         Get.toNamed(Routes.MYPAGE);
                       },
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              content: SingleChildScrollView(
+                                child: ListBody(
+                                  children: const <Widget>[
+                                    Text(
+                                      '신고하시겠습니까?\n신고하시면 해당 글은 차단되며 서버로 데이터가 전송됩니다.\n글은 관리자의 검토를 거쳐 삭제될 수 있습니다.',
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              actions: [
+                                TextButton(
+                                  child: const Text("확인"),
+                                  onPressed: () {
+                                    addreports(controller.post.postId.toString());
+                                    controller.next();
+                                    Get.back();
+                                  },
+                                ),
+                                TextButton(
+                                  child: const Text("취소"),
+                                  onPressed: () {
+                                    Get.back();
+                                  },
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      },
+                      icon: const Icon(
+                        Icons.report,
+                        color: Colors.white,
+                      ),
                     ),
                   ],
                 ),
